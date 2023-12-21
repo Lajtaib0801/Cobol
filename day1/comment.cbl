29e52afd713f5172d112d27bf0ac3e2b42e125ea
Lajtaib0801 <lajtaibenja@gmail.com>
2023-12-21 22:02:36 +0100

    day1

---
+++ day1/comment.cbl
@@ -0,0 +1,14 @@
+000100 IDENTIFICATION DIVISION.
+000200 PROGRAM-ID. comment.
+000300 ENVIRONMENT DIVISION.
+       DATA DIVISION.                                                   MB072197
+000500 PROCEDURE DIVISION.
+000600
+000700* This is a comment.                                              MB072197
+000800* This paragraph displays information about the program.          MB072197
+000900 PROGRAM-BEGIN.
+003700     DISPLAY "This program contains four DIVISIONS,".
+003800     DISPLAY "three PARAGRAPHS".
+001000     DISPLAY "and four SENTENCES".
+001100 PROGRAM-DONE.
+001200     STOP RUN.
