761fe18b335a6cf9523f49a9fdba47a5e32b9beb
Lajtaib0801 <lajtaibenja@gmail.com>
2023-12-21 22:03:24 +0100

    day1

--- day1/comment.cbl
+++ day1/comment.cbl
@@ -7,8 +7,8 @@
 000700* This is a comment.                                              MB072197
 000800* This paragraph displays information about the program.          MB072197
 000900 PROGRAM-BEGIN.
-003700     DISPLAY "This program contains four DIVISIONS,".
-003800     DISPLAY "three PARAGRAPHS".
+003700     DISPLAY "This program contains four DIVISIONS,".             MB072197
+003800     DISPLAY "three PARAGRAPHS".                                  MB072197
 001000     DISPLAY "and four SENTENCES".
 001100 PROGRAM-DONE.
 001200     STOP RUN.
